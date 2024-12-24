8c5692227e0cb1cf4632665e0b0ff34bf7b2ff02
nopphanat01 <sspas401@gmail.com>
2024-12-24 13:58:25 +0700

    11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.\n";
+    int i;
+    for (i=0;i<3;i++){
+    cin.ignore();}
+    srand(time(0));
+    string g;
+    int x = rand()%9;
+    if(x == 0) g ="A";
+    else if (x == 1) g = "B+";
+    else if (x == 2) g = "B";
+    else if (x == 3) g = "C+";
+    else if (x == 4) g = "C";
+    else if (x == 5) g = "D+";
+    else if (x == 6) g = "D";
+    else if (x == 7) g = "F";
+    else g = "W";
+    
+    cout << "You will get "<< g <<" in this 261102." ;
+    }
+
